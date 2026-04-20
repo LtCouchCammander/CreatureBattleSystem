80fe8841e0a38fb870fe3201641029d7e183d3d8
LtCouchCammander <149845440+LtCouchCammander@users.noreply.github.com>
2026-04-20 11:39:05 -0500

    Add PlainsWalker class with defend method override
    Implemented defense mechanism with damage reduction and healing.

---
+++ src/PlainsWalker.java
@@ -0,0 +1,23 @@
+public class PlainsWalker extends Creature {
+
+    @Override
+    public void defend(float incomingPower) {
+
+        // 10 % chance of reducing damage taken
+        if (Rand.randomInt(0, 10) < 1) {
+            incomingPower = incomingPower * 0.8f;
+            action = name + " defended and reduced damage taken to " + incomingPower;
+        }
+        // 20% chance of healing instead of taking damage
+        else if (Rand.randomInt(0, 10) < 2) {
+            health += incomingPower;
+            action = name + " used PlaninsWalker ability and use attack damage " + incomingPower + " to heal itself instead.";
+        }
+        else
+        {
+            action = name + " did not defend.";
+        }
+
+        health -= incomingPower;
+    }
+}
